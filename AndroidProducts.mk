9e7c24e40883567b45c224334d327f0ca8f77c08
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-11-22 18:34:34 +0530

    Aicp yo!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/aicp_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    aicp_lavender-user \
+    aicp_lavender-userdebug \
+    aicp_lavender-eng
