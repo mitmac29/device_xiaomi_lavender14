2a555263a7211e030d4c9738a8714c373a0a13c4
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-11-24 13:27:52 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_lavender-user \
-    aicp_lavender-userdebug \
-    aicp_lavender-eng
+    aicp_lavender-userdebug
